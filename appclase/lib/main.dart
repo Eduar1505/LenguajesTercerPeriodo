ecab01e0c312657475cd06724a135a56056b4258
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-10-15 18:17:20 -0600

    app

---
+++ appclase/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+        body: Column(
+          children: [
+            Text('Hola Mundo'),
+          ],
+          ),
+          
+        )
+        ),
+    );
+  }
+}
